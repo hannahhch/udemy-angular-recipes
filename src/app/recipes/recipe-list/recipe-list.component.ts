c5185f36c4f4b0dc18dd3e7e541d4851c1737c75
Hannah <hannah.hudson@smashingboxes.com>
2019-03-07 08:35:28 -0500

    finished adding recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe = [
+    new Recipe(
+      'Avocado Toast',
+      'Subtitle',
+      'https://media.self.com/photos/5a29b90fe1ef6d3e6b87ff67/4:3/w_728,c_limit/SNACKS_07_SIMPLE_AVOCADO_TOAST_037.jpg',
+    ),
+    new Recipe(
+      'Poke Bowls',
+      'Subtitle',
+      'https://www.jessicagavin.com/wp-content/uploads/2016/06/sockeye-salmon-poke-spicy-soy-sauce_1200.jpg',
+    )
+  ];
 
   constructor() { }
 
