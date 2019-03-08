15fdcf7c2e5218b39aea5fdaa3c8c56152f44f86
Hannah <hannah.hudson@smashingboxes.com>
2019-03-08 15:49:33 -0500

    added shoping list;

--- src/app/shopping-lists/shopping-lists.component.ts
+++ src/app/shopping-lists/shopping-lists.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-lists',
@@ -6,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-lists.component.scss']
 })
 export class ShoppingListsComponent implements OnInit {
-
+  ingredients: Ingredient[] = [
+    new Ingredient('Cheese', 1),
+    new Ingredient('Tomatoes', 2),
+    new Ingredient('Pizza Dough', 1),
+    new Ingredient('Pepperoni', 3),
+  ];
   constructor() { }
 
   ngOnInit() {
